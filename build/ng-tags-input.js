d57093acb1875013b95a8a3cecf6990edd8099f1
Alexis Ohayon <alexis@takes.io>
2014-04-30 20:17:03 +0200

    chore(build): Update build files [skip ci]

--- build/ng-tags-input.js
+++ build/ng-tags-input.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2013-2014 Michael Benford
  * License: MIT
  *
- * Generated at 2014-04-30 20:12:27 +0200
+ * Generated at 2014-04-30 20:17:02 +0200
  */
 (function() {
 'use strict';
@@ -1016,7 +1016,7 @@ tagsInput.run(["$templateCache", function($templateCache) {
   );
 
   $templateCache.put('ngTagsInput/auto-complete-contact.html',
-    "<div class=\"autocomplete\" ng-show=\"suggestionList.visible\"><ul class=\"suggestion-list\"><li class=\"suggestion-item\" ng-repeat=\"item in suggestionList.items track by track(item)\" ng-class=\"{selected: item == suggestionList.selected}\" ng-click=\"addSuggestion()\" ng-mouseenter=\"suggestionList.select($index)\"><div></div><div ng-bind-html=\"highlight(item)\"></div><div ng-bind-html=\"highlightContactName(item.completeName)\"></div></li></ul></div>"
+    "<div class=\"autocomplete\" ng-show=\"suggestionList.visible\"><ul class=\"suggestion-list\"><li class=\"suggestion-item\" ng-repeat=\"item in suggestionList.items track by track(item)\" ng-class=\"{selected: item == suggestionList.selected}\" ng-click=\"addSuggestion()\" ng-mouseenter=\"suggestionList.select($index)\"><div class=\"contact-picture\"><img ng-src=\"item.picture\"></div><div class=\"contact-mail\" ng-bind-html=\"highlight(item)\"></div><div class=\"contact-name\" ng-bind-html=\"highlightContactName(item.completeName)\"></div></li></ul></div>"
   );
 }]);
 
